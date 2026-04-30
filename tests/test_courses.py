ffa7911d3e400acb3dd59ceb666962035ee00400
timur100500 <99761640+timur100500@users.noreply.github.com>
2026-04-30 13:45:14 +0300

    "Знакомство с PageComponent. Практика работы с Page Component"

--- tests/test_courses.py
+++ tests/test_courses.py
@@ -1,34 +1,20 @@
-from playwright.sync_api import sync_playwright, expect, Page
 import pytest
 from pages.create_course_page import CreateCoursePage
 from pages.courses_list_page import CoursesListPage
 
-@pytest.mark.regression  # Добавили маркировку regression
-@pytest.mark.courses  # Добавили маркировку courses
-def test_empty_courses_list(chromium_page_with_state):
-    chromium_page_with_state.goto("https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/courses")
-
-    # Проверяем, что отображается заголовок "Courses"
-    courses = chromium_page_with_state.get_by_test_id('courses-list-toolbar-title-text')
-    expect(courses).to_be_visible()
-    expect(courses).to_have_text("Courses")
-
-    # Проверить наличие и текст блока "There is no results"
-    results = chromium_page_with_state.get_by_test_id('courses-list-empty-view-title-text')
-    expect(results).to_be_visible()
-    expect(results).to_have_text("There is no results")
-
-    # Проверить наличие и видимость иконки пустого блока
-    icon = chromium_page_with_state.get_by_test_id('courses-list-empty-view-icon')
-    expect(icon).to_be_visible()
-
-    # Проверить наличие и текст описания блока: "Results from the load test pipeline will be displayed here"
-    text = chromium_page_with_state.get_by_test_id('courses-list-empty-view-description-text')
-    expect(text).to_be_visible()
-    expect(text).to_have_text("Results from the load test pipeline will be displayed here")
+@pytest.mark.regression
+@pytest.mark.courses
+def test_empty_courses_list(courses_list_page: CoursesListPage):
+    courses_list_page.visit("https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/courses")
+    courses_list_page.sidebar.check_visible()
+    courses_list_page.navbar.check_visible("username")
+    courses_list_page.check_visible_courses_title()
+    courses_list_page.check_visible_create_course_button()
+    courses_list_page.check_visible_empty_view()
 
 
-@pytest.mark.regression  # Добавили маркировку regression
+
+@pytest.mark.regression
 @pytest.mark.courses
 def test_create_course(create_course_page: CreateCoursePage, courses_list_page: CoursesListPage):
     create_course_page.visit("https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/courses/create")
