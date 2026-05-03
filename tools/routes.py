674b8326a5d9cf90b563a9da7c26cc898d7096c7
nad-timur <nad-timur@users.noreply.github.com>
2026-05-03 18:44:51 +0000

    deploy: 20d16c9577735ff9ae1917d538e6c3a5894e5a8a

--- tools/routes.py
+++
@@ -1,8 +0,0 @@
-from enum import Enum
-
-class AppRoute(str, Enum):
-    LOGIN = "./#/auth/login"
-    REGISTRATION = "./#/auth/registration"
-    DASHBOARD = "./#/dashboard"
-    COURSES_CREATE = "./#/courses/create"
-    COURSES = "./#/courses"
